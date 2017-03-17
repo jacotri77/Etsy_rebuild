ad0c59b434737e3687b6bcc461be077b9f2d1b98
Erik Jacobsen <slowerthanyou@gmail.com>
2017-03-17 14:22:22 -0700

    got the feed started

--- main.js
+++ main.js
@@ -1,10 +1,35 @@
 $(document).ready(function() {
 	$.getJSON('https://api.etsy.com/v2/listings/active.js?api_key=h9oq2yf3twf4ziejn10b717i&keywords=whiskey&includes=Images,Shop&callback=?', function(data){
   console.log(data)
+
+
+
+  
+  var itemsHTML = ''
+
+  data.results.forEach(function(item){
+  item.Images.forEach(function(image){ 
+    
+  
+      var template = `
+        <div class= "smaller">
+          <img  class="smallImg" src=${image.url_fullxfull}
+          <div id="imgText">
+            <h4 class="imgTitle">${item.Shop.title}</h4>
+            <p id="maker">${item.Shop.shop_name}</p>
+            <p id="pricing">$${item.price}</p>
+          </div>
+        </div>
+      `
+
+      itemsHTML += template
+
+    })
+    })
+
+
+    $('.section1').html(itemsHTML)
+
+  })
 })
-	
-
-
-
-	
-})
+  
